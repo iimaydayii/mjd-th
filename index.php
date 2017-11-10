36e9bd5e59246ae17283b889889f8c0ed3042569
iimaydayii <32700105+iimaydayii@users.noreply.github.com>
2017-11-10 11:45:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <!-- MAKE IT RESPONSIVE -->
+    </head>
+    <body>
+        <?php echo 9999;?>
+        
+    </body>
+    
+    <!-- END BODY -->
+</html>
